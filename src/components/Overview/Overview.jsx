b492ae257225dadd5d6834ae35877543203dad93
ariamsalazar <ariamsalazar@gmail.com>
2020-02-26 15:42:13 -0400

    Commit final

--- src/components/Overview/Overview.jsx
+++ src/components/Overview/Overview.jsx
@@ -116,21 +116,21 @@ const data = [
                         </div>
                     </div>
                     <div className="box-container with-padding">
-                        <span className="box-title side">Algo daily transactions</span>
-                        <Link className="link__open">Open Chart</Link>
-                        <div className="filters__container">
-                            <Link className="filter-c">1H</Link>
-                            <Link className="filter-c">2D</Link>
-                            <Link className="filter-c">3D</Link>
-                            <Link className="filter-c">1W</Link>
-                            <Link className="filter-c">3M</Link>
-                            <Link className="filter-c">6M</Link>
-                            <Link className="filter-c">1Y</Link>
-                            <Link className="filter-c">All</Link>
-                        </div>
-                        <div className="chart__container">
-                            {renderLineChart}
-                        </div>
+                        <   span className="box-title side">Algo daily transactions</span>
+                            <Link className="link__open">Open Chart</Link>
+                            <div className="filters__container">
+                                <Link className="filter-c">1H</Link>
+                                <Link className="filter-c">2D</Link>
+                                <Link className="filter-c">3D</Link>
+                                <Link className="filter-c">1W</Link>
+                                <Link className="filter-c">3M</Link>
+                                <Link className="filter-c">6M</Link>
+                                <Link className="filter-c">1Y</Link>
+                                <Link className="filter-c">All</Link>
+                            </div>
+                            <div className="chart__container">
+                                {renderLineChart}
+                            </div>
                     </div>
                </div>
             </div>
