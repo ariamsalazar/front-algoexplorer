74751745c09caf3959738f039a1f83f4866b0874
unknown <ariamsalazar@gmail.com>
2020-03-10 06:00:29 -0400

    Select multiple

--- src/components/General/Filters.jsx
+++ src/components/General/Filters.jsx
@@ -1,21 +1,54 @@
 import React from 'react';
 import './Filters.css';
 import Link from '@material-ui/core/Button';
+import Select from "react-select";
 
+const options = [
+    { label: "Sent", value: 1 },
+    { label: "Received", value: 2 },
+    { label: "All", value: 3 },
+];
 class Filters extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+          checked: false,
+          values: []
+        };
+    }
+    onChangeCheckbox = e => {
+        const isChecked = !this.state.checked;
+        this.setState({
+          checked: isChecked,
+          values: isChecked ? options : this.state.values
+        });
+      };
+      onChange = opt => {
+        const allOptionsSelected = opt.length === options.length;
+        this.setState({
+          checked: allOptionsSelected ? true : false,
+          values: opt
+        });
+    };
     render(){
         return (
             <div className="header__list">
                 Filter: 
-                <div class="select__style">
-                    <select className="select__filter">
+                <form class="select__style">
+                    <Select
+                    isMulti
+                    onChange={this.onChange}
+                    options={options}
+                    value={this.state.values}
+                    placeholder="Type of transactions"
+                    />
+                    {/* <select className="select__filter">
                         <option value="grapefruit">Type of transactions</option>
                         <option value="grapefruit">Tranfer</option>
-                        {/* <option value="lime">Feb 25, 2020 - Feb 26, 2020</option>
-                        <option selected value="coconut">Mar 01, 2020 - Mar 26, 202</option> */}
                     </select>
-                    <div class="arrow"></div>
-                </div>
+                    <div class="arrow"></div> */}
+                </form>
                 <div className="container__pagination">
                     1-25 of 9,912
                     <Link className="prev__pagination"></Link>
